1713cacad13380678e5adae66cbbb072546385b5
sixym3 <xiao.yuan0217@gmail.com>
2025-10-10 14:07:40 -0400

    Added pipettor package

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 set -e
 
 vcs import < src/ros2.repos src
+vcs import < src/end_effectors/end_effectors.repos src/end_effectors
 sudo apt-get update
 rosdep update
-rosdep install --from-paths src --ignore-src -y
+rosdep install --from-paths src --ignore-src -y --skip-keys moveit_studio_behavior_interface --skip-keys epick_moveit_studio
